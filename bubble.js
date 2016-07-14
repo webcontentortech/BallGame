2d6bd4e716c492400370f44f0937da4e4bb797f3
Monika Bharti <monika@webcontentor.com>
2016-07-14 19:01:19 +0530

    images added + made some changes in html + js file

--- bubble.js
+++ bubble.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
-	$("#start").click(function(){
-		run();
-	});
+    var number=["images/blue.png","images/green.png","images/red.png","images/yellow.png"];
+    run();
+    //$('#main1').hide(0).delay(2000).show(0);
+    //$('#main2').hide(0).delay(3000).show(0);
+    
+//  $('#main3').hide(0).delay(4000).show(0);
+//  $('#main4').hide(0).delay(6000).show(0);
+//  $('#main5').hide(0).delay(8000).show(0);
+//  $('#main6').hide(0).delay(10000).show(0);
+//  $('#main7').hide(0).delay(12000).show(0);
+//  $('#main8').hide(0).delay(14000).show(0);
+//  $('#main9').hide(0).delay(16000).show(0);
+    function run(){
+        for (var i = 1; i <=16; i++) {
+            var colorValue=Math.floor(Math.random()*number.length);
+            document.getElementById('image'+i).src = number[colorValue];
+        }
+    }
 });
-function run(){
-	$("span").animate({marginTop: "330px"}, 80000);
-}
-var ids = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-var span = document.getElementsByClassName("element");
-	while (span.length > 0) {
-    	var i = Math.floor(Math.random() * ids.length);
-    	span[0].id = 'item-' + ids[i];
-    	ids.splice(i, 1);
-    	span = [].slice.call(span, 1);
-}
+
